f5f9e92c7016574dc6334e82f0fc1a6e02dcc5fa
CesarJZO <cesarjzo@outlook.com>
2022-09-09 19:28:53 -0500

    Cleanup code

--- simple_polygon.js
+++ simple_polygon.js
@@ -6,21 +6,20 @@ function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 41.89022, lng: 12.49223 },
         zoom: 19
-
     });
 
-    let rectCoords = [
-        { lat: 41.89099, lng: 12.49099 },
-        { lat: 41.89099, lng: 12.493559 },
-        { lat: 41.88960, lng: 12.493559 },
-        { lat: 41.88960, lng: 12.49099 },
+    const rectCoords = [
+        { lat: 41.89099, lng: 12.49099 }, // NW
+        { lat: 41.89099, lng: 12.49355 }, // NE
+        { lat: 41.88960, lng: 12.49355 }, // SE
+        { lat: 41.88960, lng: 12.49099 }, // SW
     ];
 
     polygon = new google.maps.Polygon({
         paths: rectCoords,
         strokeColor: "#5C06FF",
         strokeOpacity: 0.8,
-        strokeWeight: 2,
+        strokeWeight: 3,
         fillColor: "#5C06FF",
         fillOpacity: 0.35
     });
